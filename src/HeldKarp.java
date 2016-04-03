84e79baba38b2c2a0229d59886b28d9e63e1da62
matusvida <matusvida@gmail.com>
2016-04-03 13:45:36 +0200

    HeldKarp

--- src/HeldKarp.java
+++ src/HeldKarp.java
@@ -1,5 +1,6 @@
 import org.apache.commons.lang.ArrayUtils;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -18,6 +19,7 @@ public class HeldKarp {
         for(int i=0;i<matrix.length;i++){
             firstLine[i] = matrix[0][i];
         }
+        visitedCities.add(0);
     }
 
     public void findPath(){
@@ -25,19 +27,44 @@ public class HeldKarp {
         List<Integer> path = new ArrayList<Integer>();
         List<Integer> list = new ArrayList<Integer>();
         List<Integer> distances = new ArrayList<Integer>();
-        List<Integer> copyOfList = new ArrayList<Integer>();
+        ArrayList<Integer> copyOfList = new ArrayList<Integer>();
+        List<Integer> copyListTemp = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> perm= new ArrayList<ArrayList<Integer>>();
 
-        // VYRIESIT UKLADANIE DISTANCE A HLADAT MINIMUM V MNOZINE HODNOT DISTANCE
         for(int g=0; g<matrix.length; g++) {
-            for (int s = 1; s < matrix.length; s++) {
-                for (int j = 1; j < matrix.length; j++) {
-                    if (s != j) {
-                        list.add(j);
-                        copyOfList.add(j);
-                        setDistance(0);
-                        g(s, copyOfList);
-                        distances.add(getDistance());
-                        System.out.println(distance);
+            if(g==0) {
+                for (int s = 1; s < matrix.length; s++) {
+                    for (int j = 1; j < matrix.length; j++) {
+                        if (s != j) {
+                            list.add(j);
+                            if (copyOfList.size() > 1) {
+                                copyOfList.set(g, j);
+                            } else {
+                                copyOfList.add(g, j);
+                            }
+                            copyListTemp.addAll(copyOfList);
+
+                            perm = permutations(copyOfList);
+                            if (g < 1) {
+                                if (copyOfList.size() == 0) {
+                                    copyOfList.addAll(copyListTemp);
+                                }
+                                g(s, copyOfList);
+                                distances.add(getDistance());
+                                setDistance(0);
+                            } else {
+                                for (int t = 0; t < perm.size(); t++) {
+                                    if (copyOfList.size() == 0) {
+                                        copyOfList.addAll(copyListTemp);
+                                    }
+                                    g(s, perm.get(t));
+                                    distances.add(getDistance());
+                                    setDistance(0);
+                                }
+                            }
+                            System.out.println(distance);
+                            copyListTemp.clear();
+                        }
                     }
                 }
             }
@@ -45,6 +72,8 @@ public class HeldKarp {
             for(int indexOfMinimum=0;indexOfMinimum<distances.size();indexOfMinimum++){
                 if(distances.get(indexOfMinimum) == path.get(g)) {
                     copyOfList.add(list.get(indexOfMinimum));
+                    visitedCities.add(list.get(indexOfMinimum+1));
+                    visitedCities.add(list.get(indexOfMinimum));
                     break;
                 }
             }
@@ -56,6 +85,10 @@ public class HeldKarp {
     public void g(int index1, List<Integer> index2){
 
         int temp = 0;
+//        List<Integer> index2 = new ArrayList<Integer>();
+//        for(int k=0;k<index2List.size();k++){
+//            index2.add(k, index2List.get(k));
+//        }
         int swap=0;
         if(index2.size() == 0){
             distance += firstLine[index1];
@@ -86,6 +119,48 @@ public class HeldKarp {
 //        return distance;
     }
 
+    private List<Integer> permute(java.util.List<Integer> arr, int k){
+        List<ArrayList<Integer>> permutations = new ArrayList<ArrayList<Integer>>();
+        List<Integer> inner = new ArrayList<Integer>();
+        for(int i = k; i < arr.size(); i++){
+            java.util.Collections.swap(arr, i, k);
+            inner.addAll(arr);
+            permutations.add((ArrayList) inner);
+            permute(arr, k + 1);
+            java.util.Collections.swap(arr, k, i);
+
+        }
+        if (k == arr.size() -1){
+            System.out.println(java.util.Arrays.toString(arr.toArray()));
+        }
+        return arr;
+    }
+
+    private ArrayList<ArrayList<Integer>> permutations(ArrayList<Integer> input) {
+
+        ArrayList<ArrayList<Integer>> permutations = new ArrayList<ArrayList<Integer>>();
+
+        if (input.size() == 0) {
+            permutations.add(new ArrayList<Integer>());
+            return permutations;
+        }
+
+        Integer firstElement = input.remove(0);
+
+        ArrayList<ArrayList<Integer>> recursiveReturn = permutations(input);
+        for (ArrayList<Integer> li : recursiveReturn) {
+
+            for (int index = 0; index <= li.size(); index++) {
+                ArrayList<Integer> temp = new ArrayList<Integer>(li);
+                temp.add(index, firstElement);
+                permutations.add(temp);
+            }
+
+        }
+        return permutations;
+    }
+
+
     public void g1(int index1, List<Integer> index2) {
 
         int temp = 0;
@@ -108,29 +183,6 @@ public class HeldKarp {
         minimum = (int) Collections.min(list);
 
         return minimum;
-    }
-
-
-    class TwoDimentionalArrayList<T> extends ArrayList<ArrayList<T>> {
-        public void addToInnerArray(int index, T element) {
-            while (index >= this.size()) {
-                this.add(new ArrayList<T>());
-            }
-            this.get(index).add(element);
-        }
-
-        public void addToInnerArray(int index, int index2, T element) {
-            while (index >= this.size()) {
-                this.add(new ArrayList<T>());
-            }
-
-            ArrayList<T> inner = this.get(index);
-            while (index2 >= inner.size()) {
-                inner.add(null);
-            }
-
-            inner.set(index2, element);
-        }
     }
 
     public int getDistance() {
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Matus on 28.03.2016.
@@ -10,10 +12,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        File file = new File("vstup1.txt");
+        File file = new File("vstup.txt");
         String path = file.getAbsolutePath();
         File file1 = new File("vstup1.txt");
         String path1 = file1.getAbsolutePath();
+        int minCost = 0;
 
         List<Integer> visitedCities = new ArrayList<Integer>();
         Loader loader = new Loader(path);
@@ -27,8 +30,16 @@ public class Main {
 //
 //        Writer writer = new Writer();
 //        writer.write(visitedCities);
+
+        TravelingSalesmanHeldKarp ht = new TravelingSalesmanHeldKarp();
+        minCost = ht.minCost(matrix);
+//        new Thread(() ->{
 //
-        HeldKarp heldKarp = new HeldKarp(matrix1);
-        heldKarp.findPath();
+//        }).start();
+
+        System.out.println("Min cost is " + minCost);
+//
+        HeldKarp heldKarp = new HeldKarp(matrix);
+        //heldKarp.findPath();
     }
 }
